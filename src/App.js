c47ed0bfc20f8bfa9cdc32ab358fc42e1f0493fa
taewoo <f93335383@gmail.com>
2020-02-25 23:57:22 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TopHeader from './Trello-clone/Header';
+import BoaderHeader from './Trello-clone/BoaderHeader';
+import BoaderList from './Trello-clone/BoaderList';
+
+function App() {
+    return (
+        <>
+        <TopHeader/>
+        <BoaderHeader />
+        <BoaderList />
+        </>
+
+    );
+}
+
+export default App;
